186a6f6d0b4cb4228d89626b782c094d5e7dd45e
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2023-11-30 01:52:07 +0900

    For Render

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 #!/bin/env node
 
-console.log('hello, world');
+import net from 'net';
+
+async function main() {
+  const svr = net.createServer(socket => {
+    socket.on('data', data => {
+      console.log(socket.remoteAddress + ':' + socket.remotePort + ' => ' + data);
+    });
+    socket.on('close', () => {
+      console.log('The connection is closed');
+    });
+  //}).listen(554);
+  }).listen(5555);
+
+  //console.log('0.0.0.0:554');
+  console.log('0.0.0.0:5555');
+}
+
+await main();
